83d8e32c670f2698cf74fcb25cbb5a190e25aae3
vakhitovalex <42857963+vakhitovalex@users.noreply.github.com>
2021-04-21 13:19:14 -0700

    update create user controller with email and password

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 function getUsersInfo(req, res) {
@@ -15,8 +16,18 @@ function getUsersInfo(req, res) {
 }
 
 function createUser(req, res) {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  // const { name, about, avatar, email, password } = req.body;
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) =>
+      User.create({
+        name: req.body.name,
+        about: req.body.about,
+        avatar: req.body.avatar,
+        email: req.body.email,
+        password: hash,
+      }),
+    )
     .then((user) => {
       res.status(200).send(user);
     })
--- models/user.js
+++ models/user.js
@@ -3,19 +3,21 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Jacques Cousteau',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Explorer',
   },
   avatar: {
     type: String,
-    required: true,
+    required: false,
     validate: {
       validator(avatar) {
         const avatarRegex = /^(http|https):\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i;
@@ -23,6 +25,23 @@ const userSchema = new mongoose.Schema({
       },
       message: 'Sorry, your avatar link is incorrect!',
     },
+    default: 'https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg',
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegex.test(email);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 });
 
